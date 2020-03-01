f4fc0b1460a7526fa8da16eec9c9ef5b4d2da0bd
aaric <qlhan@incarcloud.com>
2020-03-01 15:12:58 +0800

    0.1.0-SNAPSHOT mongodb

--- src/main/java/com/incarcloud/match/App.java
+++ src/main/java/com/incarcloud/match/App.java
@@ -1,21 +1,21 @@
-package com.incarcloud;
+package com.incarcloud.match;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * 启动类
+ *
  * Created by bzheng on 2019/11/28.
  */
-@SpringBootApplication
-public class App implements CommandLineRunner {
+@SpringBootApplication(scanBasePackages = {
+        "com.incarcloud.mvc",
+        "com.incarcloud.match"
+})
+public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
-    }
-
-    @Override
-    public void run(String... args) {
-        System.out.println("start run ...");
     }
 }
